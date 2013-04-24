e3644a6e49bddfdb70c93d85c0c0cdf203e11f38
Lomu <whyun@vip.qq.com>
2013-04-24 18:09:31 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -53,7 +53,7 @@ $(function(){
                     setTimeout(function(){$("#email").poshytip('hide');},3000);
                 }
             }).error(function(){
-                    showtip('Subscribe failed, the current server looks does not support php! ','yellow');
+                    showtip('Subscribe failed!\nThe current server looks does not support php! ','yellow');
             $("#email").poshytip('show');
             setTimeout(function(){$("#email").poshytip('hide');},3000);
         });
